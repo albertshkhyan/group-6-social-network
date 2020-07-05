272899a5daaa864eb3f9d7d36b1a9f54ae5c159b
vigen86 <v_soghomonyan@mail.ru>
2020-07-05 18:27:18 +0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./common/components/containers/Header/Header";
-import "./App.css";//👍
-import "./media.css";
+
 import Dashboard from './common/components/containers/Dashboard/Dashboard';
 import Profile from "./common/components/containers/Profile/Profile";
 import News from "./common/components/containers/News/News";
 import Music from "./common/components/containers/Music/Music";
 import Settings from "./Settings/Settings";
 import Users from "./common/components/containers/Users/Users";
-
+import Message from "./common/components/sections/Message/Message";
 
 import Dialogs from './common/components/containers/Dialogs/Dialogs';
 
@@ -19,30 +18,33 @@ import {
   Route,
 } from "react-router-dom";
 
+import "./App.css";//👍
+import "./media.css";
+
 
 function App(props) {
-  console.log('props', props);
   return (
     <div className="App">
-      <Header menu={["About"]}/>
+      <Header menu={["About"]} />
       <div className="flexible">
         <Dashboard />
         <main className="main">
           <Switch>
-            <Route exact path="/profile" component={() => (
-              <Profile name="Profile page" posts={props.posts} />
-            )}/>
-            {/* <Route path="/profile">
-              <Profile name={123} />
-            </Route> */}
-            {/* <Route path="/profile/123" component={() => <h2>User ALex page</h2> }/> */}
-            <Route path="/login" component={Login}/>
-            <Route exact path="/dialogs" component={Dialogs}/>
-            <Route path="/dialogs/123" component={() => <h2>dialogs123</h2>}/>
-            <Route path="/news" component={News}/>
-            <Route path="/music" component={Music}/>
-            <Route path="/settings" component={Settings}/>
-            <Route path="/users" component={Users}/>
+            <Route exact path="/profile" render={() => (
+              <Profile name="Profile page" posts={props.state.posts} />
+            )} />
+            <Route path="/login" component={Login} />
+            <Route exact path="/dialogs" render={() =>
+              <Dialogs
+                dialogs={props.state.dialogs}
+                messages={props.state.messages}
+              />
+            } />
+            <Route path="/dialogs/123" component={() => <h2>dialogs123</h2>} />
+            <Route path="/news" component={News} />
+            <Route path="/music" component={Music} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/users" component={Users} />
           </Switch>
         </main>
       </div>
@@ -50,5 +52,4 @@ function App(props) {
     </div>
   )
 }
-export const x = 10;
 export default App;
---
+++ src/redux/state.js
@@ -0,0 +1,39 @@
+
+
+const state = {
+    posts : [
+        { id: "1", post: "new post" },
+        { id: "2", post: "blablabla" },
+        { id: "3", post: "blabla" },
+        { id: "4", post: "It's my first posts" },
+        { id: "5", post: "Hi, how are you?" },
+    ],
+    dialogs : [
+        { id: "1", name: "Lyov" },
+        { id: "2", name: "Valer" },
+        { id: "3", name: "Artem" },
+        { id: "4", name: "Aram" },
+        { id: "5", name: "Vlo" },
+        { id: "6", name: "Varuj" },
+        { id: "7", name: "Arsen" },
+        { id: "8", name: "Artak" },
+        { id: "9", name: "Aram" },
+        { id: "10", name: "Aren" },
+    
+    ],
+    messages : [
+        { id: "1", message: "Hello" },
+        { id: "2", message: "Hi" },
+        { id: "3", message: "Barev" },
+        { id: "4", message: "Privet" },
+        { id: "5", message: "Adios" },
+        { id: "6", message: "Gutentag" },
+        { id: "7", message: "Gabarjoba" },
+        { id: "8", message: "Hola" },
+        { id: "9", message: "Salut" },
+        { id: "10", message: "Bari Hachoxun" },
+    
+    ]
+}
+
+export default state;
