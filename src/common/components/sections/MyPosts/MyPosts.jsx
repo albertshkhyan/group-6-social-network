b49932b55141add994419d06dcd08d649c3183d2
armen5500 <sarmen5500gmail.com>
2020-07-08 23:31:11 +0300

    i cant do this

--- src/common/components/sections/MyPosts/MyPosts.jsx
+++ src/common/components/sections/MyPosts/MyPosts.jsx
@@ -4,47 +4,24 @@ import Post from "./Post";
 import s from "./style.module.css";
 
 const MyPosts = (props) => {
-  const postArrayComp = props.posts.map((item) => (
-    <Post key={item.id} post={item.post} id={item.id} />
-  ));
-  // const postArrayComp = posts.map(item => <div key={item.id}>hello</div>)
-
-  // let myRef = React.createRef();
-  // const foo = () => {
-  // console.log(myRef);
-  // alert( myRef.current.value)
-  // }
-
-  ////without ref
-  const foo = (event) => {
-    // console.log('event', event.target.parentElement.previousElementSibling.children[0].value);//
-    let text = event.target.value;
-    // console.log('text', text);
-
-
-
-  };
-
-  ////when need give paramat
+  const postArrayComp = props.posts.map(item => 
+    <Post key={item.id} post={item.post} id={item.id} />)
 
   return (
     <div className={s.MyPosts}>
       <h2>My posts</h2>
       <div>
-        {/* <textarea id="a1">karam inc vor  ban grem</textarea> */}
-        {/* <textarea ref={myRef}/> */}
-        {/* <textarea onChange={ function(e) {
-                    foo(e, 10)
-                } } /> */}
-
-        <textarea value="alik" onChange={foo} />
+      
+        <textarea />
       </div>
       <div>
         <button>Add post</button>
       </div>
 
-      <div className={s.postsContainer}>{postArrayComp}</div>
+      <div className={s.postsContainer}>
+        {postArrayComp}
+      </div>
     </div>
-  );
-};
+  )
+  }
 export default MyPosts;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import React from "react";
 import App from './App.js';
 // console.log('ReactDOM', ReactDOM);
 import { BrowserRouter } from "react-router-dom";
-import state, {addPost} from "./redux/state";
+import state, {addPost, updateNewPostText} from "./redux/state";
 // console.log('state', state);
-
 
 window.state = state;
 
+function renderEntireTree(state){
+
 ReactDOM.render(
   <BrowserRouter>
-    <App state={state} />
+    <App state={state} addPost ={addPost} />
   </BrowserRouter>,
   document.getElementById('root')
 );
+}
+export default renderEntireTree;
 // addPost("My post!!!!")
 
 
